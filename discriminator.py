98366cd8db405034c6225e422111f3d16a74d5f0
Sathwik Katepally <59343619+sathwik-katepally@users.noreply.github.com>
2023-08-30 20:36:07 +0530

    Create discriminator.py

---
+++ discriminator.py
@@ -0,0 +1,21 @@
+# CNN based image classifier
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+#Use untrained as yet discriminator to classify images as real or fake. Model will be trained such that it outputs positive values for real images and negative values for fake images
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print (decision)
